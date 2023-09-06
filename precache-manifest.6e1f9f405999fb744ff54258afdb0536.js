ff71b47eb5c412b9bad7bde3a20e90063be00374
RyanJMarsh <ryanjmarsh93@gmail.com>
2023-09-06 10:50:53 +0100

    Updates

---
+++ precache-manifest.6e1f9f405999fb744ff54258afdb0536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "045904f9552d22984115",
+    "url": "/mongo-news/static/css/main.8ee8e477.chunk.css"
+  },
+  {
+    "revision": "045904f9552d22984115",
+    "url": "/mongo-news/static/js/main.045904f9.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/mongo-news/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "4b17f9910a2c6fd3a60e",
+    "url": "/mongo-news/static/js/2.4b17f991.chunk.js"
+  },
+  {
+    "revision": "5a61c516157ecaaed2a5",
+    "url": "/mongo-news/static/js/runtime~main.5a61c516.js"
+  },
+  {
+    "revision": "257477042fbfdcec261aa2a6fcb4cabf",
+    "url": "/mongo-news/index.html"
+  }
+];
